56e3e8d664465c0820b8317d675e2066b877ae0a
KokiHayashi <1234.884.koki@gmail.com>
2022-04-30 00:59:23 +0900

    戻るボタン

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import national from './nationalText.json';
-import international from './internationalText.json';
+import React, { useState } from 'react';
 import { useCopyToClipboard } from 'usehooks-ts';
+import './App.css';
+import international from './internationalText.json';
+import national from './nationalText.json';
+// import useHistory from 'react-router-dom';
 
 interface AppProps {}
+
+let nationalHistory = new Array();
+let internationalHistory = new Array();
 
 function App({}: AppProps) {
   // Create the count state.
@@ -13,17 +16,38 @@ function App({}: AppProps) {
   const [nationalMotion, setNationalMotion] = useState(0);
   const [internationalMotion, setInternationalMotion] = useState(0);
   const [value, copy] = useCopyToClipboard()
+//   let nationalHistory = new Array();
+//   let internationalHistory: number[] = [];
+  let num: number = -1;
+//   const history = useHistory();
   // Create the counter (+1 every second).
   function handleNationalClick() {
-		setNationalMotion(Math.floor(Math.random()*486));
+	  	console.log(nationalHistory);
+		nationalHistory.push(nationalMotion);
+		num = Math.floor(Math.random()*486);
+		setNationalMotion(num);
+		// nationalHistory.push(num);
+		console.log(num);
   }
-	function handleInternationalClick() {
-		setInternationalMotion(Math.floor(Math.random()*306));
-		if (internationalMotion === 12){
-			setInternationalMotion(13);
+  function handleInternationalClick() {
+		internationalHistory.push(internationalMotion);
+		num = Math.floor(Math.random()*306);
+		if (num === 12){
+			num = 13;
 		}
+		setInternationalMotion(num);
+		console.log(num);
   }
 
+  function handleNationalBackwardClick() {
+	  setNationalMotion(nationalHistory.pop());
+	  console.log(nationalMotion);
+  }
+  function handleInternationalBackwardClick() {
+
+	  setInternationalMotion(internationalHistory.pop());
+	  console.log(internationalMotion);
+  }
 //   useEffect(() => {
 //     const timer = setTimeout(() => setCount(count + 1), 1000);
 //     return () => clearTimeout(timer);
@@ -35,11 +59,13 @@ function App({}: AppProps) {
 		<div>
 			<button className="button" onClick={handleNationalClick}>national motion</button>
 			<button className="copy" onClick={() => copy(national[nationalMotion])}>Copy</button>
+			<button className="backward" onClick = {handleNationalBackwardClick}>戻る</button>
 			<div className="motion">{national[nationalMotion]}</div>
 			</div>
 		<div>
 			<button className="button" onClick={handleInternationalClick}>international motion</button>
 			<button className="copy" onClick={() => copy(national[nationalMotion])}>Copy</button>
+			<button className="backward" onClick = {handleInternationalBackwardClick}>戻る</button>
 			<div className="motion">{international[internationalMotion]}</div>
 		</div>
     </div>
