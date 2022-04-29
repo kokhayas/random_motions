d997d326cc32dbf8a05fb24fe27665a542d88433
KokiHayashi <1234.884.koki@gmail.com>
2022-04-29 23:45:00 +0900

    third

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import national from './nationalText.json';
+import international from './internationalText.json';
+import { useCopyToClipboard } from 'usehooks-ts';
 
 interface AppProps {}
 
 function App({}: AppProps) {
   // Create the count state.
   const [count, setCount] = useState(0);
+  const [nationalMotion, setNationalMotion] = useState(0);
+  const [internationalMotion, setInternationalMotion] = useState(0);
+  const [value, copy] = useCopyToClipboard()
   // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
+  function handleNationalClick() {
+		setNationalMotion(Math.floor(Math.random()*486));
+  }
+	function handleInternationalClick() {
+		setInternationalMotion(Math.floor(Math.random()*306));
+		if (internationalMotion === 12){
+			setInternationalMotion(13);
+		}
+  }
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => setCount(count + 1), 1000);
+//     return () => clearTimeout(timer);
+//   }, [count, setCount]);
   // Return the App component.
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+		<h3 className="title">the University of Tokyo   UTDS</h3>
+		<div>
+			<button className="button" onClick={handleNationalClick}>national motion</button>
+			<button className="copy" onClick={() => copy(national[nationalMotion])}>Copy</button>
+			<div className="motion">{national[nationalMotion]}</div>
+			</div>
+		<div>
+			<button className="button" onClick={handleInternationalClick}>international motion</button>
+			<button className="copy" onClick={() => copy(national[nationalMotion])}>Copy</button>
+			<div className="motion">{international[internationalMotion]}</div>
+		</div>
     </div>
   );
 }
